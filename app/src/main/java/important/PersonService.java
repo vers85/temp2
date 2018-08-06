1aac0a5448d6e1d8631163dfce5f268bfe598ce5
marcin <vers85@gmail.com>
2018-08-06 23:19:46 +0200

    initial

---
+++ app/src/main/java/important/PersonService.java
@@ -0,0 +1,10 @@
+package important;
+
+import com.google.common.base.Strings;
+import sample.Person;
+
+public class PersonService {
+    public Person getPerson() {
+        return new Person("a", "b");
+    }
+}
